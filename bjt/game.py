b77d47bba5e66c547ca9405edde29439353c63a7
Alejandro <amondragonflrs@gmail.com>
2022-06-15 21:13:03 -0500

    Add display card

--- bjt/game.py
+++ bjt/game.py
@@ -3,6 +3,18 @@ import random
 import configparser
 
 
+def display_card(rank: str, suit: str) -> None:
+    print('┌─────────┐')
+    print(f'│{rank}        │') if rank != "10" else print(f'│{rank}       │')
+    print('│         │')
+    print('│         │')
+    print(f'│    {suit}    │')
+    print('│         │')
+    print('│         │')
+    print(f'│        {rank}│') if rank != "10" else print(f'│       {rank}│')
+    print('└─────────┘')
+
+
 def main():
     pass
 
